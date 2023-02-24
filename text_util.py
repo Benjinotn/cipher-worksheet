077a16f1cdc9f66fdd03502ac2132ddbe39ce5f4
Ben Norton - Admin <bnorton.admin@Corp.Reference.Build>
2023-02-24 12:37:43 +0000

    added library to save clean text to text files

---
+++ text_util.py
@@ -0,0 +1,25 @@
+# reduces everything to alphanumeric characters, to make encryption functions as simple as possible
+
+def clean(text):
+    clean_text = ""
+    
+    accepted_characters = "abcdefghijklmnopqrstuvwxyz ,."
+    
+    for char in text:
+        if char.lower() not in accepted_characters:
+            continue
+        else:
+            clean_text += char.lower()
+    
+    return clean_text
+
+def save(filename, text):
+    
+    text = clean(text)
+    
+    name = filename + ".txt"
+    textfile = open(name, "w")
+    
+    textfile.write(text)
+    
+    textfile.close()
